bf482459a363748855a80be671e96112dbe481ed
verachens <verachens@gmail.com>
2020-10-18 17:37:56 +0800

    update unit test for Story 2.1

--- src/ImageTesting/Services/ImageManager.test.ts
+++ src/ImageTesting/Services/ImageManager.test.ts
@@ -4,30 +4,59 @@ import ImagePropertyProvider from "../../Image/Services/ImagePropertyProvider"
 import { ImageManager } from "../../Image/Services/ImageManager"
 import { OperationResult } from '../../common/Model/OperationResult';
 import { OperationResultEnums } from '../../common/Model/OperationResultEnums';
-import { IImageFile } from '../../Image/Models/ImageFile';
+import { FileDto } from '../../Image/Models/FileDto';
+import ArchiveHelperService from '../../Image/Services/ArchiveHelperService';
+import { FileArchiveDto } from '../../Image/Models/FileArchiveDto';
+import { StatusCodes } from 'http-status-codes';
 
 describe("ImageManager", () => {
   let mockImageRepo: ImageRepository;
   let mockImageWrapperSvc: ImagePropertyProvider;
+  let mockArchiveHelperSvc: ArchiveHelperService;
   let target: ImageManager;
 
   beforeEach(() => {
     mockImageRepo = createSpyFromClass(ImageRepository);
     mockImageWrapperSvc = createSpyFromClass(ImagePropertyProvider);
-    target = new ImageManager(mockImageRepo, mockImageWrapperSvc);
-  })
+    mockArchiveHelperSvc = createSpyFromClass(ArchiveHelperService);
+    target = new ImageManager(mockImageRepo, mockImageWrapperSvc, mockArchiveHelperSvc);
+  });
 
   it("when addImage is called with image-type file, it should save the image", async () => {
     (<Spy<ImagePropertyProvider>>mockImageWrapperSvc).getImageType.and.returnValue("image/type");
     (<Spy<ImageRepository>>mockImageRepo).addImage.and.resolveWith(new OperationResult("John Doe", OperationResultEnums.Success, "Foobar"));
-    const result = await target.addImage({ fileBlob: {data: Buffer.alloc(0)}  } as IImageFile);
+    
+    const result = await target.addImage({ fileBlob: {data: Buffer.alloc(0)}  } as FileDto);
+    
     expect(result.status === OperationResultEnums.Success).toBeTrue();
-  })
+  });
 
   it("when addImage is called with non image-type, it should return failed result", async () => {
     (<Spy<ImagePropertyProvider>>mockImageWrapperSvc).getImageType.and.returnValue(null);
-    (<Spy<ImageRepository>>mockImageRepo).addImage.and.resolveWith(new OperationResult("John Doe", OperationResultEnums.Success, "Foobar"));
-    const result = await target.addImage({ fileBlob: {data: Buffer.alloc(0)}  } as IImageFile);
+    
+    const result = await target.addImage({ fileBlob: {data: Buffer.alloc(0)}  } as FileDto);
+    
     expect(result.status === OperationResultEnums.Failed).toBeTrue();
-  })
-})
+  });
+
+  it("when getImageById is called with valid id, it should return the image data", async () => {
+    const expectedBlob = { fileBlob: { data: Buffer.alloc(0), mimeType: "image/type" }};
+    (<Spy<ImageRepository>>mockImageRepo).getImageById.and.resolveWith(expectedBlob);
+    
+    const result = await target.getImage("johnDoe");
+    
+    expect(result.getdata() === expectedBlob.fileBlob.data).toBeTrue();
+    expect(result.getContentType() === expectedBlob.fileBlob.mimeType).toBeTrue();
+  });
+
+  it("when addImagesFromArchive is called with valid archive, it should return the upload results", async () => {
+    const expectedArchiveFiles = [{ fileBlob: { data: Buffer.alloc(0) }} as FileDto];
+    (<Spy<ArchiveHelperService>>mockArchiveHelperSvc).getFilesFromArchive.and.resolveWith(expectedArchiveFiles);
+    (<Spy<ImageRepository>>mockImageRepo).addImage.and.resolveWith(new OperationResult("John Doe", OperationResultEnums.Success, "Foobar"));
+    
+    const result = await target.addImagesFromArchive({} as FileArchiveDto);
+
+    expect(result.getStatus() === StatusCodes.MULTI_STATUS).toBeTrue();
+    expect(result.getdata()).toBeDefined();
+  });
+});
